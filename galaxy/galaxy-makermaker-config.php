c5d24c1f882b50bf3a1b1b2e1a8333f14c015725
prospect <prospect.ogujiuba@gmail.com>
2025-01-05 16:19:19 -0500

    plugin updates

--- galaxy/galaxy-makermaker-config.php
+++ galaxy/galaxy-makermaker-config.php
@@ -1,10 +1,11 @@
 <?php
-$typerocket = '/var/www/html/compasscareerscanada/public/wp-content/plugins/typerocket-pro-v6/typerocket';
-$overrides = '/var/www/html/compasscareerscanada/public/wp-content/plugins/makermaker';
+$sitename = 'playground';
+$typerocket = '/var/www/html/' . $sitename . '/public/wp-content/plugins/typerocket-pro-v6/typerocket';
+$overrides = '/var/www/html/' . $sitename . '/public/wp-content/plugins/makermaker';
 define('TYPEROCKET_GALAXY_MAKE_NAMESPACE', 'MakerMaker');
 define('TYPEROCKET_GALAXY_PATH', $typerocket);
-define('TYPEROCKET_CORE_CONFIG_PATH', $typerocket . '/config' );
-define('TYPEROCKET_ROOT_WP', '/var/www/html/compasscareerscanada/public/');
+define('TYPEROCKET_CORE_CONFIG_PATH', $typerocket . '/config');
+define('TYPEROCKET_ROOT_WP', '/var/www/html/' . $sitename . '/public/');
 
 define('TYPEROCKET_APP_ROOT_PATH', $overrides);
 define('TYPEROCKET_ALT_PATH', $overrides);
