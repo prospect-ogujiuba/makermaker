96999b9e181b34a38bc717ac500297b04e091538
prospect <prospect.ogujiuba@gmail.com>
2025-01-05 19:05:51 -0500

    makermaker updates

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MakerMaker\Models;
 
 use TypeRocket\Models\Model;
@@ -7,11 +8,7 @@ class Status extends Model
 {
     protected $resource = 'makermaker_statuses';
 
-    // Remove primaryKey override as it should use 'id'
-    // protected $primaryKey = 'code';
-
     protected $fillable = [
-        'id',
         'code',
         'description'
     ];
@@ -20,5 +17,4 @@ class Status extends Model
         'code' => 'required|string|max:20|unique',
         'description' => 'required|string|max:100'
     ];
-
 }
