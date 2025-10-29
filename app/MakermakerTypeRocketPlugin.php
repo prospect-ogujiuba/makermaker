8b4cd202551ad8a9daf132201b293de25de701f2
prospect-ogujiuba <prospect.ogujiuba@gmail.com>
2025-10-29 15:00:15 -0400

    update the BasePlugin and makermaker-config to be dynamic

--- app/MakermakerTypeRocketPlugin.php
+++ app/MakermakerTypeRocketPlugin.php
@@ -17,16 +17,92 @@ class MakermakerTypeRocketPlugin extends BasePlugin
 
     public function init()
     {
+        $this->loadResources();
+        $this->setupSettingsPage();
+        $this->registerAssets();
+    }
 
-        // Include Resource files here (Typically Creation of custom resource and adding pages)
-        $resources = [
-        ];
 
-        foreach ($resources as $resource) {
+    public function routes()
+    {
+        include MAKERMAKER_PLUGIN_DIR . 'inc/routes/api.php';
+        include MAKERMAKER_PLUGIN_DIR . 'inc/routes/public.php';
+    }
 
-            include MAKERMAKER_PLUGIN_DIR . 'inc/resources/' . $resource . '.php';
+    public function policies()
+    {
+        return $this->discoverPolicies();
+    }
+
+    public function activate()
+    {
+        $this->migrateUp();
+        System::updateSiteState('flush_rewrite_rules');
+    }
+
+    public function deactivate()
+    {
+        System::updateSiteState('flush_rewrite_rules');
+    }
+
+    public function uninstall()
+    {
+        $this->migrateDown();
+    }
+
+    private function loadResources()
+    {
+        $resources_dir = MAKERMAKER_PLUGIN_DIR . 'inc/resources';
+
+        if (!is_dir($resources_dir)) {
+            return;
         }
 
+        // Get all PHP files
+        $resource_files = glob($resources_dir . '/*.php');
+
+        if (empty($resource_files)) {
+            return;
+        }
+
+        // Include each resource file
+        foreach ($resource_files as $file) {
+            include_once $file;
+        }
+    }
+
+    private function discoverPolicies()
+    {
+        $policies = [];
+        $policies_dir = MAKERMAKER_PLUGIN_DIR . 'app/Auth';
+
+        if (!is_dir($policies_dir)) {
+            return $policies;
+        }
+
+        $policy_files = glob($policies_dir . '/*Policy.php');
+
+        foreach ($policy_files as $file) {
+            $filename = basename($file, '.php');
+
+            // Extract model name (e.g., ProductPolicy -> Product)
+            if (preg_match('/^(.+)Policy$/', $filename, $matches)) {
+                $model_name = $matches[1];
+                $model_class = '\\MakerMaker\\Models\\' . $model_name;
+                $policy_class = '\\MakerMaker\\Auth\\' . $filename;
+
+                // Only register if model exists
+                if (class_exists($model_class) && class_exists($policy_class)) {
+                    $policies[$model_class] = $policy_class;
+                }
+            }
+        }
+
+        return $policies;
+    }
+
+    private function setupSettingsPage()
+    {
         // Plugin Settings
         $page = $this->pluginSettingsPage([
             'view' => View::new('settings', [
@@ -39,7 +115,10 @@ class MakermakerTypeRocketPlugin extends BasePlugin
                 'settings' => "<a href=\"{$page->getUrl()}\" aria-label=\"Settings\">Settings</a>"
             ];
         });
+    }
 
+    private function registerAssets()
+    {
         // Assets Manifest
         $manifest = $this->manifest('public');
         $uri = $this->uri('public');
@@ -55,45 +134,5 @@ class MakermakerTypeRocketPlugin extends BasePlugin
             wp_enqueue_style('admin-style-' . $this->slug, $uri . $manifest['/admin/admin.css']);
             wp_enqueue_script('admin-script-' . $this->slug, $uri . $manifest['/admin/admin.js'], [], false, true);
         });
-
-        // TODO: Add your init code here
-    }
-
-
-    public function routes()
-    {
-        include MAKERMAKER_PLUGIN_DIR . 'inc/routes/api.php';
-        include MAKERMAKER_PLUGIN_DIR . 'inc/routes/public.php';
-    }
-
-    public function policies()
-    {
-        // TODO: Add your TypeRocket policies here
-        return [
-
-        ];
-    }
-
-    public function activate()
-    {
-        $this->migrateUp();
-        System::updateSiteState('flush_rewrite_rules');
-
-        // TODO: Add your plugin activation code here
-    }
-
-    public function deactivate()
-    {
-        // Migrate `down` only on plugin uninstall
-        System::updateSiteState('flush_rewrite_rules');
-
-        // TODO: Add your plugin deactivation code here
-    }
-
-    public function uninstall()
-    {
-        $this->migrateDown();
-
-        // TODO: Add your plugin uninstall code here
     }
 }
